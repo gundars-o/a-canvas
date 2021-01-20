ea645ffe92a78ae0962871c98b532c4410f77faf
gundars-o <gundars.ozolins75@gmail.com>
2021-01-20 14:57:06 +0200

    Draw lines

--- canvas.js
+++ canvas.js
@@ -1,3 +1,8 @@
 var canvas = document.querySelector( "canvas" );
 var c = canvas.getContext( "2d" );
 c.fillRect( 10, 30, 50, 100 );
+// Lines
+c.beginPath();
+c.moveTo( 9, 29 );
+c.lineTo( 61, 29 );
+c.stroke();
