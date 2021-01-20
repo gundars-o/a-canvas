881e301a226db2ae7210591f6e9d03505e364483
gundars-o <gundars.ozolins75@gmail.com>
2021-01-20 15:19:05 +0200

    Do not strech canvas. Set canvas size to get one px is one px.

--- canvas.js
+++ canvas.js
@@ -1,8 +1,19 @@
 var canvas = document.querySelector( "canvas" );
+window.addEventListener( "resize", setCanvasSize );
 var c = canvas.getContext( "2d" );
-c.fillRect( 10, 30, 50, 100 );
-// Lines
-c.beginPath();
-c.moveTo( 9, 29 );
-c.lineTo( 61, 29 );
-c.stroke();
+function setCanvasSize() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    console.log( canvas.width );
+    console.log( canvas.height );
+    drawOnCanvas()
+};
+function drawOnCanvas() {
+    c.fillRect( 10, 30, 50, 100 );
+    // Lines
+    c.beginPath();
+    c.moveTo( 9, 29 );
+    c.lineTo( 61, 29 );
+    c.stroke();
+};
+setCanvasSize();
