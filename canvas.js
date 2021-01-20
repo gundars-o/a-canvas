02a7b7565a4100b3e8e5ba45b4b1117eba36a5ad
gundars-o <gundars.ozolins75@gmail.com>
2021-01-20 16:05:32 +0200

    Move circle.

--- canvas.js
+++ canvas.js
@@ -5,7 +5,7 @@ setCanvasSize();
 function setCanvasSize() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    drawOnCanvas()
+    drawOnCanvas();
 };
 function drawOnCanvas() {
     c.fillStyle = "rgba( 255, 0, 0, 0.5 )";
@@ -33,4 +33,16 @@ function drawOnCanvas() {
         c.strokeStyle = "blue";
         c.stroke();
     };
+    // Move Circle
+    var x = 200;
+    function animate() {
+        requestAnimationFrame( animate );
+        c.clearRect( 0, 0, innerWidth, innerHeight );
+        c.beginPath();
+        c.arc( x, 200, 30, 0, Math.PI * 2, false );
+        c.strokeStyle = "blue";
+        c.stroke();
+        x += 1;
+    };
+    animate();
 };
